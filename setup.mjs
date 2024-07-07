0e9b1f9b948b9a716112f15e60ddf7ca9ea4986f
Filippo Alessandrini <filippoalessandrini.fa@gmail.com>
2024-07-07 21:17:32 +0200

    Updated setup.

--- setup.mjs
+++ setup.mjs
@@ -1,19 +1,4 @@
-import { promises as fs } from 'fs';
-import path from 'path';
 import dotenv from 'dotenv';
-
-const deleteFolderRecursive = async (path) => {
-  const stat = await fs.stat(path);
-  if (stat.isDirectory()) {
-    const files = await fs.readdir(path);
-    await Promise.all(
-      files.map((file) => deleteFolderRecursive(`${path}/${file}`))
-    );
-    await fs.rmdir(path);
-  } else {
-    await fs.unlink(path);
-  }
-};
 
 (async () => {
   dotenv.config();
