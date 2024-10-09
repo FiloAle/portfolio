47a3e1331dd9a2e95ba1a5b5bcaa4c807ccdbf2b
Filippo Alessandrini <filippoalessandrini.fa@gmail.com>
2024-10-09 12:32:54 +0200

    Updated infos.

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -21,7 +21,7 @@ export default function ExperiencePage() {
           Student, since 2021
         </p>
         <p>
-          I started my design experience at <a href="https://polimi.it" target="_blank">Politecnico di Milano</a> more
+          I've started my design experience at <a href="https://polimi.it" target="_blank">Politecnico di Milano</a> more
           than three years ago getting to know with Communication Design. Now, after my BA graduation, I'm happy to continue
           my path at PoliMi attending an MSc about Digital and Interaction Design.
         </p>
@@ -30,19 +30,42 @@ export default function ExperiencePage() {
             Digital and Interaction Design MSc <span className="text-neutral-600 dark:text-neutral-400">(since 2024)</span>
           </li>
           <li>
-            Communication Design BA <span className="text-neutral-600 dark:text-neutral-400">(2021-2024)</span>
+            Communication Design BA <span className="text-neutral-600 dark:text-neutral-400">(2021 – 2024)</span>
+          </li>
+        </ul>
+
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">CopySystem SnC</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Marketing & Communication strategist, 2022 – 2023
+        </p>
+        <p>
+          During my second year at Politecnico di Milano, I've had the opportunity to apply my
+          communication design skills in a real world case scenario by working at CopySystem.
+          There, I've been collaborating with a workgroup in order to deliver both digital and
+          physical artifacts, continuously developing a deeper knowledge in fields regarding:
+        </p>
+        <ul>
+          <li>
+            Graphic design
+          </li>
+          <li>
+            Editorial design
+          </li>
+          <li>
+            Marketing
           </li>
         </ul>
 
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
         <h2 className="font-medium text-xl mb-1 tracking-tighter">ITT Blaise Pascal</h2>
         <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          Student, 2016-2021
+          Student, 2016 – 2021
         </p>
         <p>
           My passion for technology led me to study Information Technology and Telecoms 
           at <a href="https://www.ispascalcomandini.it/itt-b-pascal/" target="_blank">ITT Blaise Pascal</a>,
-          where I had the opportunity to develop several skills and methods I'm still making advantage of nowadays.
+          where I've had the opportunity to develop several skills and methods I'm still making advantage of nowadays.
           There, I've learned the basics of Computer Science and coding, having to deal with different tools and
           programming languages such as:
         </p>
